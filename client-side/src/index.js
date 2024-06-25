18592c46c9828972d47632d5fbe5764e918294a3
ahsherazkhan <109746604+ahsherazkhan@users.noreply.github.com>
2024-06-26 02:30:19 +0500

    Update index.js

--- client-side/src/index.js
+++ client-side/src/index.js
@@ -11,7 +11,7 @@ import {
 } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'https://dandy-backend-razbnef48-ahsherazkhans-projects.vercel.app',
+  uri: 'http://localhost:4000',
   cache: new InMemoryCache()
 
 });
